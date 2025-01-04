6be2dc1f70b2d56de3da773e348765d2567f7334
hardwoong <dew04300@naver.com>
2025-01-05 01:29:22 +0900

    add learn-express

---
+++ learn-express/routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const router = express.Router();
+
+// GET / 라우터
+// router.get('/', (req, res) => {
+//     res.send('Hello, Express');
+// });
+
+router.get('/', (req, res, next) => {
+    res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
